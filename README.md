25183602acf07cc2ab6395b9672e3fa77073a5db
mohammadharbi <mohammad.a.aljagthmi@gmail.com>
2020-03-11 20:31:11 +0300

    marvel game is done

---
+++ README.md
@@ -0,0 +1,89 @@
+<<<<<<< HEAD
+
+
+-- Developer Mohammad Aljagthmi  - Wed 11th, 03 2020
+=================================================
+
+-- Technology used thorugh out the projects:
+  - HTML, CSS, JS and JQ. 
+  - My wireframwork is drawn manully on a paper. (will be attached too). 
+
+-- Important notes:
+  - My project contains two versions of the same game which have levels of hard and easy. 
+  - I prefered to use two separate files than using hide and show function to experinece the difference. 
+  - There is a page that will show you your level in the process after a user complete the game either in easy or hard level. 
+  - A challange is made by the developer that the first user gets 6/8 as a score in the hard level will be awarded 100 SAR. 
+
+-- Screenshots of my design on a paper:
+
+-- wireframes:
+
+-- Instruction of downloading the project:
+   >> copy the url of the repo and git clone it in prefered folder. 
+   >> update as you like and git init then git add it. 
+   >> after that, do commit with a explained message and push it to me. 
+   >> I will check if your changes are deserved to be merged to my project when I see a pull request.
+
+-- There is no unsolved problems but I think I tried to avoid them with more complex way. 
+-- Some of my function:
+   >> The difference between the hard level and easy level is the shuffle function that I use to shuffle cards each game, and that's why I call my game is a very hard, luck and nervy game. 
+   >> play and pause music are worked in a way that exactly I want, and I used differet technique to do it but calling the function in the HTML code. 
+   >> I had an idea to put a challange in a news style and I searched in Google in how to do that which ended up creating 4 divs inside each other with a very complex and weired css stylling.
+
+   🐈. 
+
+
+  
+=======
+
+
+-- Developer Mohammad Aljagthmi  - Wed 11th, 03 2020
+=================================================
+
+-- Technology used thorugh out the projects:
+  - HTML, CSS, JS and JQ. 
+  - My wireframwork is drawn manully on a paper. (will be attached too). 
+
+-- Important notes:
+  - My project contains two versions of the same game which have levels of hard and easy. 
+  - I prefered to use two separate files than using hide and show function to experinece the difference. 
+  - There is a page that will show you your level in the process after a user complete the game either in easy or hard level. 
+  - A challange is made by the developer that the first user gets 6/8 as a score in the hard level will be awarded 100 SAR. 
+
+-- Screenshots of my design on a paper:
+
+
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/FirstPage.jpeg)
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/SecondPage.jpeg)
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/ThirdPage.jpeg)
+![alt text](https://git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/blob/master/MarvelGame/images/FourthPage.jpeg?raw=true)
+
+
+
+
+
+
+-- wireframes:
+
+
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/thumbnail_Image.jpg)
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/thumbnail_Image-1.jpg)
+![alt text](https://raw.git.generalassemb.ly/mohammadharbi/M-KMarvelGame-Mohammad/master/MarvelGame/images/thumbnail_Image-2.jpg)
+
+-- Instruction of downloading the project:
+   >> copy the url of the repo and git clone it in prefered folder. 
+   >> update as you like and git init then git add it. 
+   >> after that, do commit with a explained message and push it to me. 
+   >> I will check if your changes are deserved to be merged to my project when I see a pull request.
+
+-- There is no unsolved problems but I think I tried to avoid them with more complex way. 
+-- Some of my function:
+   >> The difference between the hard level and easy level is the shuffle function that I use to shuffle cards each game, and that's why I call my game is a very hard, luck and nervy game. 
+   >> play and pause music are worked in a way that exactly I want, and I used differet technique to do it but calling the function in the HTML code. 
+   >> I had an idea to put a challange in a news style and I searched in Google in how to do that which ended up creating 4 divs inside each other with a very complex and weired css stylling.
+
+   🐈. 
+
+
+  
+>>>>>>> 80c1ea951b9e257f318929a7ab8a9ad849841920
---
+++ js/mainEasyGame.js
@@ -0,0 +1,242 @@
+
+let houseOfCards = [
+    {
+    
+        character:"spiderman",
+        image: "./images/spiderMan1.png"
+    },
+    {
+        character: "spiderman",
+        image: "./images/spiderMan2.jpg"
+    },
+    {
+        character: "hulk",
+        image: "./images/hulk1.jpg"
+    },
+    {
+        character: "hulk",
+        image: "./images/hulk2.jpg"
+    },
+    {
+        character: "batman",
+        image: "./images/batman1.jpg"
+    },
+    {
+        character: "batman",
+        image: "./images/batman2.jpg"
+    
+    },
+    {
+        character: "ironman",
+        image: "./images/iron1.jpg"
+    },
+    {
+        character: "ironman",
+        image: "./images/iron2.jpg"
+    },
+    {
+        character: "CaptianOfAmerica",
+        image: "./images/COFA1.jpg"
+    },
+    {
+        character: "CaptianOfAmerica",
+        image: "./images/COFA2.jpg"
+    },
+    {
+        character: "blackWidow",
+        image: "./images/black1.jpg"
+    },
+    {
+        character: "blackWidow",
+        image: "./images/back2.jpg"
+    },
+    {
+        character: "theGreen",
+        image: "./images/greenL1.jpg"
+    },
+    {
+        character: "theGreen",
+        image: "./images/greenL2.jpg"
+    },
+    {
+        character: "superMan",
+        image: "./images/superman1.jpg"
+    },
+    {
+        character: "superMan",
+        image: "./images/superman2.jpg"
+    }
+    ]
+    
+    
+    $('#marvelPlayer').hide();
+    $('#level3').hide();
+    $('#level2').hide();
+    $('#level1').hide();
+    $('#loser').hide();
+    $('#rematch').hide();
+      
+    let arrayHolder = [];
+    
+    let matchCards = [];
+    
+    let totatMatch = 0;
+    
+    function playGame(){
+    
+       // let listner = document.querySelector(".imageSQPP");
+       let score = document.querySelector(".total");
+       let move = this.getAttribute('id');
+       arrayHolder.push(houseOfCards[move].character);
+       this.setAttribute('src', houseOfCards[move].image);
+    
+       if(arrayHolder.length === 2){      
+           if(arrayHolder[0] === arrayHolder[1]){
+               totatMatch = totatMatch + 1;
+               score.textContent = "Score: " + totatMatch;
+           }
+           else if(arrayHolder[0] !== arrayHolder[1]){
+            totatMatch = totatMatch - 1;
+            score.textContent = "Score: " + totatMatch;
+           }
+           arrayHolder = [];
+       }
+    }
+    
+    // function to display home. 
+    function displayStage (){
+            let home = document.querySelector(".cardsMar");
+        
+            for(let i= 0; i < houseOfCards.length; i++){
+                let images = document.createElement("img");
+                images.setAttribute("class", "imageSQPP");
+                images.setAttribute("src", "./images/marvel.png");
+                images.setAttribute('id', i);
+                images.addEventListener('click', playGame);
+                images.style.height = " 250px";
+                images.style.width = "180px";
+                home.appendChild(images);
+            }
+    }
+    displayStage();
+    
+    // create a timer for playing the game. once it 00:00, the page will be reset.
+    function displayTimer(){
+        let container = document.querySelector(".time");
+        let anotherContainer = document.querySelector(".date");
+        let newP = document.createElement("p");
+        let newD = document.createElement("p");
+        newP.setAttribute("class", "timer");
+        newD.setAttribute("class", "dater");
+        
+        let date = new Date();
+        let months = ["January", "February", "March", "April",
+         "May", "June", "July", "August", 
+         "September", "October", "November", "December"];
+         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let day = date.getDay();
+        let month = date.getMonth();
+        let year = date.getFullYear()
+        let today = date.getDate();
+        var ampm = hour >= 12 ? ' PM' : ' AM';
+        if(hour < 10){
+            hour = "0"+date.getHours();
+        }
+        if(min < 10){
+            min = "0"+date.getMinutes();
+        }
+        // let sPage = document.querySelector(".timeContainer");
+        let greeting = document.createElement("p");
+        let history = document.createElement("p");
+        greeting.setAttribute("class", "greet");
+        history.setAttribute("class", "history");
+    
+        newP.textContent = hour + ":" + min + ampm;
+        history.textContent = days[day] + "," + " " + today + " " + months[month] + " " + year;
+       
+        
+        container.appendChild(newP)
+        anotherContainer.appendChild(history);
+    }
+    displayTimer();
+    
+    function startTimer(duration, display) {
+        
+        let timer = duration, minutes, seconds;
+        let timeText = document.querySelector("#time");
+    
+        
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+    
+            if (minutes == 0 && seconds == 0){
+                if(totatMatch > 6){
+                    $('#homePlaying').hide();
+                    $('#marvelPlayer').show();
+                    $('#rematch').show();
+                    $('#timerGoDown').hide();
+                }else if(totatMatch == 1 || totatMatch == 2){
+                    $('#homePlaying').hide();
+                    $('#level1').show();
+                    $('#rematch').show();
+                    $('#timerGoDown').hide();
+                }else if(totatMatch == 3 || totatMatch == 4){
+                    $('#homePlaying').hide();
+                    $('#level2').show();
+                    $('#rematch').show();
+                    $('#timerGoDown').hide();
+                }else if(totatMatch == 5 || totatMatch == 6){
+                    $('#homePlaying').hide();
+                    $('#level3').show();
+                    $('#rematch').show();
+                    $('#timerGoDown').hide();
+                }else if(totatMatch <= 0){
+                    $('#homePlaying').hide();
+                    $('#loser').show();
+                    $('#rematch').show();
+                    $('#timerGoDown').hide();
+                }
+               
+            }
+    
+            if(minutes == 0 && seconds == 30){ // the timer will turn red with bigger font if reachs to 00:30
+                timeText.style.color = "red";
+                timeText.style.fontSize = "40px";
+            }
+         
+            if(minutes < 10){
+                minutes = "0"+ minutes;
+            }
+            if(seconds < 10){
+                seconds = "0" + seconds;
+            }
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+    
+    window.onload = function () {
+        let theOneMinutes = 60;
+        display = document.querySelector('#time');
+        startTimer(theOneMinutes, display);
+    };
+    
+    let audio = document.querySelector(".audio");
+    
+    
+    function playMusic(){
+        audio.play();
+        audio.loop = true; // to loop the music when is ended. 
+    }
+    function pauseMusic(){
+        audio.pause();
+    }
+    
+ 
